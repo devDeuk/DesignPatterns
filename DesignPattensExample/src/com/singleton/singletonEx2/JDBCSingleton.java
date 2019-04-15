7e17c8f61ef663e06fb4e0edd19e7d8969548967
devDeuk <devDeuk@DESKTOP-30112E8>
2019-04-15 23:36:36 +0900

    싱글톤

--- DesignPattensExample/src/com/singleton/singletonEx2/JDBCSingleton.java
+++ DesignPattensExample/src/com/singleton/singletonEx2/JDBCSingleton.java
@@ -2,6 +2,9 @@ package com.singleton.singletonEx2;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 class JDBCSingleton {
 	//1단계
@@ -17,17 +20,98 @@ class JDBCSingleton {
 	 public static JDBCSingleton getInstance() {
 		if(instance == null) {
 			instance = new JDBCSingleton();
+		}else {
+			System.out.println("이미 instance 생성되어있음");
 		}
 		return instance;
 	}
 
-	private static Connection getConneion() {
+	private static Connection getConnection() {
 		
 		Connection con = null;
-		//Class.forName("");
-		//con=DriverManager.getConnection("", user, password);
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			con=DriverManager.getConnection("jdbc:mysql://localhost:3306/kubdb", "kub", "xmsxms2");
+			
+			System.out.println("Mysql 데이타베이스 접속");			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 		return con;
 	}
-	 
 	
+	
+    public int insert(String name, String pass) throws SQLException{  
+        Connection con=null;  
+        PreparedStatement ps=null;  
+        int recordCounter=0;  
+          
+        try {  
+            con=this.getConnection();  
+            ps=con.prepareStatement("insert into userdata(uname,upassword)values(?,?)");  
+            ps.setString(1, name);  
+            ps.setString(2, pass);  
+            recordCounter=ps.executeUpdate();  
+                  
+        } catch (Exception e) { e.printStackTrace(); } finally{  
+              if (ps!=null){  
+                ps.close();  
+            }if(con!=null){  
+                con.close();  
+            }   
+        }  
+       return recordCounter;  
+    }  	
+	
+    
+    public  void view(String name) throws SQLException  
+    {  
+    	Connection con = null;  
+    	PreparedStatement ps = null;  
+    	ResultSet rs = null;  
+                
+    	try {  
+                    
+          con=this.getConnection();  
+          ps=con.prepareStatement("select * from userdata where uname=?");  
+          ps.setString(1, name);  
+          rs=ps.executeQuery();  
+          while (rs.next()) {  
+            System.out.println("Name= "+rs.getString(1)+"\t"+"Paasword= "+rs.getString(2));      
+          }  
+              
+        } catch (Exception e){ 
+        	System.out.println(e);
+        }finally{  
+	      if(rs!=null){  
+	          rs.close();  
+	      }if (ps!=null){  
+	        ps.close();  
+	      }if(con!=null){  
+	    	  con.close();  
+	      }   
+        }  
+    }
+    
+    public int update(String name, String password) throws SQLException  {  
+        Connection con=null;  
+        PreparedStatement ps=null;  
+        int recordCounter=0;  
+        try {  
+	        con=this.getConnection();  
+	        ps=con.prepareStatement(" update userdata set upassword=? where uname='"+name+"' ");  
+	        ps.setString(1, password);  
+	        recordCounter=ps.executeUpdate();  
+        } catch (Exception e) {  e.printStackTrace(); } finally{  
+                
+            if (ps!=null){  
+                ps.close();  
+            }if(con!=null){  
+                con.close();  
+            }   
+         }  
+       return recordCounter;  
+    }  
+
+    
 }
---
+++ DesignPattensExample/src/com/singleton/singletonEx2/JDBCSingletonDemo.java
@@ -0,0 +1,46 @@
+package com.singleton.singletonEx2;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.sql.SQLException;
+
+public class JDBCSingletonDemo {
+
+	public static void main(String[] args) {
+		
+		JDBCSingleton jdbcSingleton = JDBCSingleton.getInstance();
+		
+		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+		
+		try {
+			System.out.print("이름을 입력하세요");
+			String username = br.readLine();
+
+			System.out.print("비밀번호를 입력하세요");
+			String userpass = br.readLine();
+			
+			try {
+				int i = jdbcSingleton.insert(username, userpass);
+				System.out.println( "i :"+ i);
+				
+				if(i > 0) {
+					System.out.println( "정상으로 저장하였습니다");
+				}else {
+					System.out.println( "저장하지 못했습니다.");
+				}
+				
+				jdbcSingleton.view(username);
+				
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+			
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		
+	}
+
+}
